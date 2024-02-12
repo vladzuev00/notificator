9f82a783c0c9a1dee2082fe068597ada14948472
Vlad Zuev <vladzuev.00@mail.ru>
2024-02-12 13:19:09 +0300

    add UnitRepository

---
+++ src/main/java/by/aurorasoft/notificator/crud/repository/UnitRepository.java
@@ -0,0 +1,8 @@
+package by.aurorasoft.notificator.crud.repository;
+
+import by.aurorasoft.notificator.crud.entity.UnitEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UnitRepository extends JpaRepository<UnitEntity, Long> {
+
+}
