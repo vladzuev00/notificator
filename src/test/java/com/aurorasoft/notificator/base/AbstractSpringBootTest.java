9adc15a869494c97d92c5abd312544c0e32404a5
Vlad Zuev <vladzuev.00@mail.ru>
2024-07-08 15:35:18 +0300

    refactor AbstractSpringBootTest

--- src/test/java/com/aurorasoft/notificator/base/AbstractSpringBootTest.java
+++ src/test/java/com/aurorasoft/notificator/base/AbstractSpringBootTest.java
@@ -5,11 +5,9 @@ import com.aurorasoft.notificator.base.containerinitializer.KafkaContainerInitia
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.BeforeClass;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.BeforeAll;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 import static java.time.ZoneOffset.UTC;
@@ -19,14 +17,13 @@ import static java.util.TimeZone.setDefault;
 @Slf4j
 @Transactional
 @SpringBootTest
-@RunWith(SpringRunner.class)
 @ContextConfiguration(initializers = {DataBaseContainerInitializer.class, KafkaContainerInitializer.class})
 public abstract class AbstractSpringBootTest {
 
     @PersistenceContext
     protected EntityManager entityManager;
 
-    @BeforeClass
+    @BeforeAll
     public static void setDefaultTimeZone() {
         setDefault(getTimeZone(UTC));
     }
