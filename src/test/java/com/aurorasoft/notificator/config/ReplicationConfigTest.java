cd606ca8b653d19946081a63be70733ccc330c3b
Vlad Zuev <vladzuev.00@mail.ru>
2024-07-08 14:27:38 +0300

    add ReplicationConfigTest

--- src/test/java/com/aurorasoft/notificator/config/ReplicationConfigTest.java
+++ src/test/java/com/aurorasoft/notificator/config/ReplicationConfigTest.java
@@ -1,47 +1,57 @@
 package com.aurorasoft.notificator.config;
 
 import by.aurorasoft.replicator.model.pipeline.ReplicationConsumePipeline;
+import com.aurorasoft.notificator.base.AbstractSpringBootTest;
 import com.aurorasoft.notificator.crud.entity.GeofenceEntity;
 import com.aurorasoft.notificator.crud.repository.GeofenceRepository;
 import org.apache.kafka.common.serialization.LongDeserializer;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import static org.junit.Assert.*;
-import static org.mockito.Mockito.mock;
 
-public final class ReplicationConfigTest {
-    private final ReplicationConfig config = new ReplicationConfig();
+public final class ReplicationConfigTest extends AbstractSpringBootTest {
+
+    @Autowired
+    private LongDeserializer longDeserializer;
+
+    @Autowired
+    @Qualifier("geofencePipeline")
+    private ReplicationConsumePipeline<GeofenceEntity, Long> geofencePipeline;
+
+    @Value("${replication.consume.topic.geofence}")
+    private String geofenceTopic;
+
+    @Autowired
+    private GeofenceRepository geofenceRepository;
 
     @Test
     public void longDeserializerShouldBeCreated() {
-        LongDeserializer actual = config.longDeserializer();
-        assertNotNull(actual);
+        assertNotNull(longDeserializer);
     }
 
     @Test
     public void geofencePipelineShouldBeCreated() {
-        String givenTopic = "geofence-sync";
-        GeofenceRepository givenRepository = mock(GeofenceRepository.class);
+        assertNotNull(geofencePipeline);
 
-        ReplicationConsumePipeline<GeofenceEntity, Long> actual = config.geofencePipeline(givenTopic, givenRepository);
-        assertNotNull(actual);
+        String actualTopic = geofencePipeline.getTopic();
+        assertSame(geofenceTopic, actualTopic);
 
-        String actualTopic = actual.getTopic();
-        assertSame(givenTopic, actualTopic);
-
-        Class<?> actualDeserializerType = actual.getIdDeserializer().getClass();
+        Class<?> actualDeserializerType = geofencePipeline.getIdDeserializer().getClass();
         Class<?> expectedDeserializerType = LongDeserializer.class;
         assertSame(expectedDeserializerType, actualDeserializerType);
 
-        String actualTypeName = actual.getReplicationTypeReference().getType().getTypeName();
+        String actualTypeName = geofencePipeline.getReplicationTypeReference().getType().getTypeName();
         String expectedTypeName = "by.aurorasoft.replicator.model.replication.consumed.ConsumedReplication<"
                 + "com.aurorasoft.notificator.crud.entity.GeofenceEntity, "
                 + "java.lang.Long"
                 + ">";
         assertEquals(expectedTypeName, actualTypeName);
 
-        JpaRepository<GeofenceEntity, Long> actualRepository = actual.getRepository();
-        assertSame(givenRepository, actualRepository);
+        JpaRepository<GeofenceEntity, Long> actualRepository = geofencePipeline.getRepository();
+        assertSame(geofenceRepository, actualRepository);
     }
 }
