8e8a0d45e9b2d435206a46af53b8a0dc5253c310
Vlad Zuev <vladzuev.00@mail.ru>
2024-07-08 13:34:32 +0300

    refactor ReplicationConfig and add ReplicationConfigTest

--- src/main/java/com/aurorasoft/notificator/config/ReplicationConfig.java
+++ src/main/java/com/aurorasoft/notificator/config/ReplicationConfig.java
@@ -5,25 +5,29 @@ import by.aurorasoft.replicator.model.pipeline.ReplicationConsumePipeline;
 import com.aurorasoft.notificator.crud.entity.GeofenceEntity;
 import com.aurorasoft.notificator.crud.repository.GeofenceRepository;
 import com.fasterxml.jackson.core.type.TypeReference;
+import org.apache.kafka.common.serialization.LongDeserializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import static org.apache.kafka.common.serialization.Serdes.Long;
-
 @Configuration
 @EnableReplication
-public class ReplicationConsumeConfig {
+public class ReplicationConfig {
+
+    @Bean
+    public LongDeserializer longDeserializer() {
+        return new LongDeserializer();
+    }
 
     @Bean
     public ReplicationConsumePipeline<GeofenceEntity, Long> geofencePipeline(@Value("${replication.consume.topic.geofence}") String topic,
                                                                              GeofenceRepository repository) {
-        return ReplicationConsumePipeline.<GeofenceEntity, Long>builder()
-                .topic(topic)
-                .idSerde(Long())
-                .replicationTypeReference(new TypeReference<>() {
-                })
-                .repository(repository)
-                .build();
+        return new ReplicationConsumePipeline<>(
+                topic,
+                longDeserializer(),
+                new TypeReference<>() {
+                },
+                repository
+        );
     }
 }
