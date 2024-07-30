bcd7c2bcd5a166d18d94425459592c673e7e5a1d
Vlad Zuev <vladzuev.00@mail.ru>
2024-07-30 15:55:35 +0300

    add ObjectMapperConfigTest

---
+++ src/test/java/com/aurorasoft/notificator/config/ObjectMapperConfigTest.java
@@ -0,0 +1,19 @@
+package com.aurorasoft.notificator.config;
+
+import com.aurorasoft.notificator.base.AbstractSpringBootTest;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public final class ObjectMapperConfigTest extends AbstractSpringBootTest {
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    public void objectMapperShouldBeCreated() {
+        assertNotNull(objectMapper);
+    }
+}
