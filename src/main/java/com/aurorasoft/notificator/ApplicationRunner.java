f43379e2217b0f9619711ba4b0ff95de48398f73
Vlad Zuev <vladzuev.00@mail.ru>
2024-07-30 16:10:43 +0300

    refactor ApplicationRunner

--- src/main/java/com/aurorasoft/notificator/ApplicationRunner.java
+++ src/main/java/com/aurorasoft/notificator/ApplicationRunner.java
@@ -9,7 +9,7 @@ import static org.springframework.boot.SpringApplication.run;
 @ConfigurationPropertiesScan
 public class ApplicationRunner {
 
-    public static void main(String... args) {
+    public static void main(String[] args) {
         run(ApplicationRunner.class, args);
     }
 }
