c0af55afbb52c4296521b39441178f0f12df5052
Vlad Zuev <vladzuev.00@mail.ru>
2024-07-31 11:40:48 +0300

    add ReplicationConfig::telegramChatPipeline

--- src/main/java/com/aurorasoft/notificator/config/ReplicationConfig.java
+++ src/main/java/com/aurorasoft/notificator/config/ReplicationConfig.java
@@ -3,8 +3,10 @@ package com.aurorasoft.notificator.config;
 import by.aurorasoft.replicator.annotation.EnableReplication;
 import by.aurorasoft.replicator.model.pipeline.ReplicationConsumePipeline;
 import com.aurorasoft.notificator.crud.entity.GeofenceEntity;
+import com.aurorasoft.notificator.crud.entity.TelegramChatEntity;
 import com.aurorasoft.notificator.crud.entity.TimeZoneEntity;
 import com.aurorasoft.notificator.crud.repository.GeofenceRepository;
+import com.aurorasoft.notificator.crud.repository.TelegramChatRepository;
 import com.aurorasoft.notificator.crud.repository.TimeZoneRepository;
 import com.fasterxml.jackson.core.type.TypeReference;
 import org.apache.kafka.common.serialization.LongDeserializer;
@@ -36,6 +38,18 @@ public class ReplicationConfig {
     @Bean
     public ReplicationConsumePipeline<TimeZoneEntity, Long> timeZonePipeline(@Value("${replication.consume.topic.time-zone}") String topic,
                                                                              TimeZoneRepository repository) {
+        return new ReplicationConsumePipeline<>(
+                topic,
+                longDeserializer(),
+                new TypeReference<>() {
+                },
+                repository
+        );
+    }
+
+    @Bean
+    public ReplicationConsumePipeline<TelegramChatEntity, Long> telegramChatPipeline(@Value("${replication.consume.topic.telegram-chat}") String topic,
+                                                                                     TelegramChatRepository repository) {
         return new ReplicationConsumePipeline<>(
                 topic,
                 longDeserializer(),
