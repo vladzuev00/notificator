b872252014c99e51b2fd0ead468169e46b05b1d8
Vlad Zuev <vladzuev.00@mail.ru>
2024-07-31 11:59:13 +0300

    refactor ReplicationConfigTest

--- src/test/java/com/aurorasoft/notificator/config/ReplicationConfigTest.java
+++ src/test/java/com/aurorasoft/notificator/config/ReplicationConfigTest.java
@@ -3,14 +3,15 @@ package com.aurorasoft.notificator.config;
 import by.aurorasoft.replicator.model.pipeline.ReplicationConsumePipeline;
 import com.aurorasoft.notificator.base.AbstractSpringBootTest;
 import com.aurorasoft.notificator.crud.entity.GeofenceEntity;
+import com.aurorasoft.notificator.crud.entity.TelegramChatEntity;
 import com.aurorasoft.notificator.crud.entity.TimeZoneEntity;
 import com.aurorasoft.notificator.crud.repository.GeofenceRepository;
+import com.aurorasoft.notificator.crud.repository.TelegramChatRepository;
 import com.aurorasoft.notificator.crud.repository.TimeZoneRepository;
 import org.apache.kafka.common.serialization.Deserializer;
 import org.apache.kafka.common.serialization.LongDeserializer;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -22,12 +23,13 @@ public final class ReplicationConfigTest extends AbstractSpringBootTest {
     private LongDeserializer longDeserializer;
 
     @Autowired
-    @Qualifier("geofencePipeline")
     private ReplicationConsumePipeline<GeofenceEntity, Long> geofencePipeline;
 
     @Autowired
-    @Qualifier("timeZonePipeline")
     private ReplicationConsumePipeline<TimeZoneEntity, Long> timeZonePipeline;
+
+    @Autowired
+    private ReplicationConsumePipeline<TelegramChatEntity, Long> telegramChatPipeline;
 
     @Value("${replication.consume.topic.geofence}")
     private String geofenceTopic;
@@ -35,11 +37,17 @@ public final class ReplicationConfigTest extends AbstractSpringBootTest {
     @Value("${replication.consume.topic.time-zone}")
     private String timeZoneTopic;
 
+    @Value("${replication.consume.topic.telegram-chat}")
+    private String telegramChatTopic;
+
     @Autowired
     private GeofenceRepository geofenceRepository;
 
     @Autowired
     private TimeZoneRepository timeZoneRepository;
+
+    @Autowired
+    private TelegramChatRepository telegramChatRepository;
 
     @Test
     public void longDeserializerShouldBeCreated() {
@@ -65,6 +73,17 @@ public final class ReplicationConfigTest extends AbstractSpringBootTest {
                 LongDeserializer.class,
                 "by.aurorasoft.replicator.model.replication.consumed.ConsumedReplication<com.aurorasoft.notificator.crud.entity.TimeZoneEntity, java.lang.Long>",
                 timeZoneRepository
+        );
+    }
+
+    @Test
+    public void telegramChatPipelineShouldBeCreated() {
+        verifyPipeline(
+                telegramChatPipeline,
+                telegramChatTopic,
+                LongDeserializer.class,
+                "by.aurorasoft.replicator.model.replication.consumed.ConsumedReplication<com.aurorasoft.notificator.crud.entity.TelegramChatEntity, java.lang.Long>",
+                telegramChatRepository
         );
     }
 
