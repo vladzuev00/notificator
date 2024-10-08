51bc7cbccb2cfbbf7ab6d1a00b8f5913c18fa534
Vlad Zuev <vladzuev.00@mail.ru>
2024-10-08 11:03:31 +0300

    refactor ReplicationConfig and ReplicationConfigTest

--- src/main/java/com/aurorasoft/notificator/config/ReplicationConfig.java
+++ src/main/java/com/aurorasoft/notificator/config/ReplicationConfig.java
@@ -3,11 +3,7 @@ package com.aurorasoft.notificator.config;
 import by.aurorasoft.replicator.annotation.EnableReplication;
 import by.aurorasoft.replicator.model.setting.ReplicationConsumeSetting;
 import com.aurorasoft.notificator.crud.entity.GeofenceEntity;
-import com.aurorasoft.notificator.crud.entity.TelegramChatEntity;
-import com.aurorasoft.notificator.crud.entity.TimeZoneEntity;
 import com.aurorasoft.notificator.crud.repository.GeofenceRepository;
-import com.aurorasoft.notificator.crud.repository.TelegramChatRepository;
-import com.aurorasoft.notificator.crud.repository.TimeZoneRepository;
 import com.fasterxml.jackson.core.type.TypeReference;
 import org.apache.kafka.common.serialization.LongDeserializer;
 import org.springframework.beans.factory.annotation.Value;
@@ -36,30 +32,4 @@ public class ReplicationConfig {
                 }
         );
     }
-
-//    @Bean
-//    public ReplicationConsumeSetting<TimeZoneEntity, Long> timeZoneReplicationConsumeSetting(
-//            @Value("${replication.consume.topic.time-zone}") String topic,
-//            TimeZoneRepository repository
-//    ) {
-//        return new ReplicationConsumeSetting<>(
-//                topic,
-//                repository,
-//                longDeserializer(),
-//                new TypeReference<>() {
-//                }
-//        );
-//    }
-//
-//    @Bean
-//    public ReplicationConsumePipeline<TelegramChatEntity, Long> telegramChatPipeline(@Value("${replication.consume.topic.telegram-chat}") String topic,
-//                                                                                     TelegramChatRepository repository) {
-//        return new ReplicationConsumePipeline<>(
-//                topic,
-//                longDeserializer(),
-//                new TypeReference<>() {
-//                },
-//                repository
-//        );
-//    }
 }
