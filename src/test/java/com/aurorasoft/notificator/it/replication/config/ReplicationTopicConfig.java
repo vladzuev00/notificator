fdcfeb2f440f79e8ac03ab584b6cbb3ef6244c58
Vlad Zuev <vladzuev.00@mail.ru>
2024-10-10 16:50:47 +0300

    add ReplicationTopicConfig::unitReplicationTopic

--- src/test/java/com/aurorasoft/notificator/it/replication/config/ReplicationTopicConfig.java
+++ src/test/java/com/aurorasoft/notificator/it/replication/config/ReplicationTopicConfig.java
@@ -7,9 +7,20 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ReplicationTopicConfig {
+    private static final int TOPIC_PARTITIONS_COUNT = 1;
+    private static final short TOPIC_REPLICATION_FACTOR = 1;
 
     @Bean
     public NewTopic geofenceReplicationTopic(@Value("${replication.consume.topic.geofence}") String name) {
-        return new NewTopic(name, 1, (short) 1);
+        return createTopic(name);
+    }
+
+    @Bean
+    public NewTopic unitReplicationTopic(@Value("${replication.consume.topic.unit}") String name) {
+        return createTopic(name);
+    }
+
+    private NewTopic createTopic(String name) {
+        return new NewTopic(name, TOPIC_PARTITIONS_COUNT, TOPIC_REPLICATION_FACTOR);
     }
 }
